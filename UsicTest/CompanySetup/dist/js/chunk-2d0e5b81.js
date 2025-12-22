637752de9c8063b1ad19a885f36c07908c672a24
abanga627 <110024694+abanga627@users.noreply.github.com>
2025-12-22 18:27:24 +0530

    changes

---
+++ UsicTest/CompanySetup/dist/js/chunk-2d0e5b81.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5b81"],{"964b":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this;n._self._c;return n._m(0)},l=[function(){var n=this,e=n._self._c;return e("div",[e("h1",[n._v("hello")])])}],c=t("2877"),r={},s=Object(c["a"])(r,u,l,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e5b81.js.map
